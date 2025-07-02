12022f1359d5419e82e046eb206e1452e2b6bdd4
juanjor0 <juanjorodriguez682@gmail.com>
2025-07-02 17:57:05 +0200

    Fix: build completa copiada correctamente al template de Django

--- config/urls.py
+++ config/urls.py
@@ -1,28 +1,36 @@
-
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
-from core.views import BoardViewSet, TaskViewSet,logout_view
-from core import views
-from django.views.generic import TemplateView
-from django.urls import re_path
-
+from core.views import BoardViewSet, TaskViewSet, logout_view
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from django.views.generic import TemplateView
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
+# View que carga React sin CSRF (para evitar conflictos)
+class FrontendAppView(TemplateView):
+    template_name = "index.html"
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+# API routing
 router = DefaultRouter()
 router.register(r'boards', BoardViewSet, basename='board')
 router.register(r'tasks', TaskViewSet, basename='task')
 
+# URL patterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('logout/', logout_view, name='logout'),
     path('', include('core.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
-urlpatterns += [
-    re_path(r'^.*$', TemplateView.as_view(template_name="index.html")),
+    
+    # Esta ruta SIEMPRE al final: sirve React para cualquier ruta no encontrada
+    re_path(r'^.*$', FrontendAppView.as_view(), name='frontend'),
 ]
