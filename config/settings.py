4b3bd9d3407231598d941c674c05a97acc2ab78e
JuanjoR0 <163863322+JuanjoR0@users.noreply.github.com>
2025-07-02 15:42:25 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from datetime import timedelta
 import os
 import mimetypes
+import dj_database_url
 mimetypes.add_type("application/javascript", ".js", True)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -61,10 +62,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Base de datos
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 # Validadores de contraseña
