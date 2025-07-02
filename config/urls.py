b524d34d7603035671611b7245e038a097f822b4
JuanjoR0 <163863322+JuanjoR0@users.noreply.github.com>
2025-07-02 17:11:56 +0200

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,8 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from core.views import BoardViewSet, TaskViewSet,logout_view
 from core import views
+from django.views.generic import TemplateView
+from django.urls import re_path
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -20,4 +22,7 @@ urlpatterns = [
     path('', include('core.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
+urlpatterns += [
+    re_path(r'^.*$', TemplateView.as_view(template_name="index.html")),
 ]
